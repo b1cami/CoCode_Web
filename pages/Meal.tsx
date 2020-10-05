d6b8c51a302ebac6065087c9bbce5a1d5ab55e0c
yiyb0603 <yiyb0603@gmail.com>
2020-10-05 10:47:10 +0900

    [FIX] Structures

--- pages/Meal.tsx
+++ pages/Meal.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import 'styles/Default.scss';
-import { Provider } from 'mobx-react';
-import stores from 'stores';
 import PageTemplate from 'components/Common/PageTemplate';
 import MealContainer from 'containers/Meal';
 
 const MealPage = () => {
 	return (
-		<Provider store={stores}>
-			<PageTemplate>
-				<MealContainer />
-			</PageTemplate>
-		</Provider>
+		<PageTemplate>
+			<MealContainer />
+		</PageTemplate>
 	);
 };
 
--- pages/Post.tsx
+++ pages/Post.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import 'styles/Default.scss';
-import { Provider } from 'mobx-react';
-import stores from 'stores';
 import PageTemplate from 'components/Common/PageTemplate/PageTemplate';
 import CommunityContainer from 'containers/Community';
 
 const PostPage = () => {
 	return (
-		<Provider store={stores}>
-			<PageTemplate>
-				<CommunityContainer />
-			</PageTemplate>
-		</Provider>
+		<PageTemplate>
+			<CommunityContainer />
+		</PageTemplate>
 	);
 };
 
